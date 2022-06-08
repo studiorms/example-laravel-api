c76214e0e03a57b1221d574c50534725149a6e1a
Robert Smith <roberts@studiorms.com>
2022-06-08 13:44:37 -0700

    Added authenticate routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Models\User;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,22 @@ use App\Http\Controllers\ArticleController;
 |
 */
 
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('articles', ArticleController::class);
+Route::apiResource('articles', ArticleController::class)->only([
+    'index',
+    'show'
+]);
+
+Route::middleware('auth:sanctum')->apiResource('articles', ArticleController::class)->only([
+    'store',
+    'update',
+    'destroy'
+]);
